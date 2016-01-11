1a60df729328b858100c0a4fb7412f0d0e7ff028
francesco086 <francesco086@gmail.com>
2016-01-12 00:07:46 +0100

    Commit Zero

---
+++ src/Estimators.hpp
@@ -0,0 +1,35 @@
+#ifndef ESTIMATORS
+#define ESTIMATORS
+
+namespace mci
+{
+   // Compute average and standard deviation (error) of a set of data x[N], assuming that they are not correlated
+   void UncorrelatedEstimator(const long &n, const double * x, double * average, double * error);
+
+   // Compute average and error, using the blocking technique
+   void BlockEstimator(const long &n, const double * x, const int &nblocks, double * average, double * error);
+
+   // Compute average and error for correlated data, using the blocking technique
+   void CorrelatedEstimator(const long &n, const double * x, double * average, double * error);
+
+
+   // Multidimensional data set
+   // Useful when you have an array of data sets
+   // Data are supposed to be organized in this way:
+   // data[n][ndim]
+   // i.e. data[i][0] contains the element i for the first data set of which one wants to compute average and error
+   //      data[i][1] for the second data set, etc.
+   // The output will be given by arrays: average[ndim] and error[ndim]
+
+   // Compute average and standard deviation (error) of a set of data x[N], assuming that they are not correlated
+   void MultiDimUncorrelatedEstimator(const long &n, const int &ndim, const double * const * x, double * average, double *error);
+
+   // Compute average and error, using the blocking technique
+   void MultiDimBlockEstimator(const long &n, const int &ndim, const double * const * x, const int &nblocks, double * average, double * error);
+
+   // Compute average and error for correlated data, using the blocking technique
+   void MultiDimCorrelatedEstimator(const long &n, const int &ndim, const double * const * x, double * average, double * error);
+
+}
+
+#endif
---
+++ src/MCISamplingFunctionInterface.hpp
@@ -0,0 +1,53 @@
+#ifndef MCI_SAMPLING_FUNCTION_INTERFACE
+#define MCI_SAMPLING_FUNCTION_INTERFACE
+
+ 
+class MCISamplingFunctionInterface
+{
+   protected:
+      int _ndim; //dimension of the input array (walker position)
+      int _nproto; //number of proto sampling functions given as output
+      double * _protonew; //array containing the new proto sampling functions
+      double * _protoold; //array containing the old proto sampling functions
+
+   public:
+      MCISamplingFunctionInterface(const int &ndim, const int &nproto)
+      {
+         _ndim=ndim; _nproto=nproto;
+         _protonew = new double[nproto]; _protoold = new double[nproto];
+         for (int i=0; i<nproto; ++i){ _protonew[i]=0.; }
+         for (int i=0; i<nproto; ++i){ _protoold[i]=0.; }
+      }
+      virtual ~MCISamplingFunctionInterface()
+      {
+         delete[] _protonew; delete[] _protoold;
+      }
+
+      // Getters
+      int getNDim(){ return _ndim;}
+      int getNProto(){ return _nproto;}
+      double getProtoNew(const int &i){ return _protonew[i]; }
+      double getProtoOld(const int &i){ return _protoold[i]; }
+
+      // Utilities
+      void newToOld()
+      {
+         for(int i=0; i<_nproto; ++i){ double * foo; foo=_protonew; _protonew=_protoold; _protoold=foo; }
+      }
+
+      void computeNewSamplingFunction(const double * in)
+      {
+         samplingFunction(in,_protonew);
+      }
+
+      // --- METHODS THAT MUST BE IMPLEMENTED
+      // Function that MCI uses for the proto-sampling function. Computes _protonew
+      virtual void samplingFunction(const double *in, double * protovalues) = 0;
+      //                                      ^walker position  ^resulting proto-values
+      
+      // Acceptance function, that uses the new and old values of the proto sampling function
+      virtual double getAcceptance() = 0;
+};
+
+
+#endif
