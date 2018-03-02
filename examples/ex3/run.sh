04628f4607dd6f32e6962926d872394bd9ed5409
francesco086 <francesco086@gmail.com>
2018-03-02 17:31:49 +0100

    built ex3 for showing how to use a call back function

---
+++ examples/ex3/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+source ../../config.sh
+OS_NAME=$(uname)
+
+# Delete old compiled files
+\rm -f exe
+\rm -f *.o
+
+#runtime dynamic library path
+RPATH="$(pwd)/../../:${FFNN_FOLDER}"
+
+FLAGS_TO_USE=$OPTFLAGS
+
+# Build the main executable
+echo "$CC $FLAGS $FLAGS_TO_USE -I$(pwd)/../../src/ ${IFFNN} -c *.cpp"
+$CC $FLAGS $FLAGS_TO_USE -Wall -I$(pwd)/../../src/ ${IFFNN} -c *.cpp
+
+case ${OS_NAME} in
+    "Darwin")
+        echo "$CC $FLAGS $FLAGS_TO_USE -L$(pwd)/../.. ${LFFNN} -o exe *.o -l${LIBNAME} ${LIBFFNN}"
+        $CC $FLAGS $FLAGS_TO_USE -L$(pwd)/../.. ${LFFNN} -o exe *.o -l${LIBNAME} ${LIBFFNN}
+        ;;
+    "Linux")
+        echo "$CC $FLAGS $FLAGS_TO_USE -L$(pwd)/../.. ${LFFNN} -Wl,-rpath=${RPATH} -o exe *.o -l${LIBNAME} ${LIBFFNN}"
+        $CC $FLAGS $FLAGS_TO_USE -L$(pwd)/../.. ${LFFNN} -Wl,-rpath=${RPATH} -o exe *.o -l${LIBNAME} ${LIBFFNN}
+        ;;
+esac
+
+
+./exe
