e06e5a8d14d86de68f2b52ec7cc3a73c78efd1ec
Jan Kessler <jakessle@t-online.de>
2018-07-11 23:05:50 +0200

    Adapted the config template to changed ffnn lib location

--- config_template.sh
+++ config_template.sh
@@ -16,8 +16,8 @@ OPTFLAGS="-O3 -flto"
 DEBUGFLAGS="-g -O0"
 
 #FFNN Library (used in ex3)
-FFNN_FOLDER="/...../FeedForwardNeuralNetwork"
-IFFNN="-I${FFNN_FOLDER}/include/"
+FFNN_FOLDER="/...../FeedForwardNeuralNetwork/lib/.libs"
+IFFNN="-I${FFNN_FOLDER}/../../include/"
 LFFNN="-L${FFNN_FOLDER}"
 LIBNAMEFFNN="ffnn"
 LIBFFNN="-lffnn"
